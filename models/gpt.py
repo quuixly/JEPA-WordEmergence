d2fc5203b2e3120a7b5bdb4d59517196a328af6e
Kamil quuixly <quuixly@gmail.com>
2026-02-19 21:04:59 +0100

    refactor: Explain the input to the forward method in more detail

--- models/gpt.py
+++ models/gpt.py
@@ -12,14 +12,14 @@ class Embedding(nn.Module):
         self.weights = nn.Parameter(torch.randn(vocabulary_size, dimension), requires_grad=True)
 
     def forward(self, x):
-        # Return vectors for each word x_i in x
-        # x.shape = (batch_size, x_n)
-        # return shape = (batch_size, x_n, dimension)
+        # Return vectors for each word in x
+        # x.shape = (batch_size, input_sequence_length)
+        # return shape = (batch_size, input_sequence_length, dimension)
         return self.weights[x]
 
 
 class GPT(nn.Module):
-    def __init__(self, dimension=512, num_heads=8, num_layers=4, vocabulary_size=61, sequence_length=61):
+    def __init__(self, dimension=512, num_heads=8, num_layers=4, vocabulary_size=61, max_sequence_length=61):
         super().__init__()
 
 
