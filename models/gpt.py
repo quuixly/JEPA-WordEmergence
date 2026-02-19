cdf6b029af76fdf8c17d4764ff09384e5f327cc7
Kamil quuixly <quuixly@gmail.com>
2026-02-19 21:51:53 +0100

    feat: Add positional encoding

--- models/gpt.py
+++ models/gpt.py
@@ -18,10 +18,34 @@ class Embedding(nn.Module):
         return self.weights[x]
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, max_sequence_length, dimension):
+        super().__init__()
+        self.max_sequence_length = max_sequence_length
+        self.dimension = dimension
+        # In the original "Attention Is All You Need" paper, sinusoidal positional encodings were used.
+        # Here, we use learnable positional embeddings.
+        self.weights = nn.Parameter(torch.zeros(max_sequence_length, dimension), requires_grad=True)
+
+    def forward(self, x):
+        # x.shape (bath_size, input_sequence_length, dimension)
+        # return shape = (batch_size, input_sequence_length, dimension)
+        if x.dim() == 2: # Without batches
+            input_sequence_length = x.shape[0]
+            positional_vectors = self.weights[:input_sequence_length, :]
+
+            return x + positional_vectors
+        elif x.dim() == 3:
+            batch_size, input_sequence_length, dimension = x.shape
+
+            positional_vectors = self.weights[:input_sequence_length, :]
+            # Explicit broadcasting (input_sequence_length, dimension) to align the batch_size
+            positional_vectors = positional_vectors.unsqueeze(0).expand(batch_size, input_sequence_length, dimension)
+
+            return x + positional_vectors
+        else:
+            raise ValueError("x must be 2D or 3D tensor")
+
 class GPT(nn.Module):
     def __init__(self, dimension=512, num_heads=8, num_layers=4, vocabulary_size=61, max_sequence_length=61):
         super().__init__()
-
-
-if __name__ == "__main__":
-    pass
